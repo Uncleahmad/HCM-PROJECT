4bf0f409839d23cc4e93e1f991a5dfbf1686c763
Uncleahmad <161314214+Uncleahmad@users.noreply.github.com>
2025-12-02 12:37:48 +0100

    Setting up project

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HcmProjectController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// HcmProject Routes
+Route::get('/hcm.ng/home',[HcmProjectController::class, 'index'])->name('home');
+Route::get('/hcm.ng/about', [HcmProjectController::class, 'about'])->name('about');
+Route::get('/hcm.ng/team', [HcmProjectController::class, 'team'])->name('team');
+Route::get('/hcm.ng/careers', [HcmProjectController::class, 'careers'])->name('careers');
+Route::get('/hcm.ng/internship', [HcmProjectController::class, 'internship'])->name('internship');
+Route::get('/hcm.ng/newsroom', [HcmProjectController::class, 'newsroom'])->name('newsroom');
+
+
+
